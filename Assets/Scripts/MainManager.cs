f034738b4d8071dca338b8b041ebf0fb9b6d91e5
ud7192 <ud7192@g.rit.edu>
2024-06-13 13:30:58 -0700

    updated color

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class MainManager : MonoBehaviour
 {
     public static MainManager Instance;
+    public Color TeamColor;
     private void Awake()
     {
+        //singleton pattern
+        if (Instance != null)
+        {
+            Destroy(Instance);
+            return;
+        }
+
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
